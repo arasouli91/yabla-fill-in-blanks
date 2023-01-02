578735a3d78cd4958d8f96e4b2212c4e602dcf9a
Amir Rasouli <arasouli91@gmail.com>
2023-01-02 13:18:07 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,402 @@
+var CARDS = [
+  {
+    front: "\u5766\u627f<br>t\u01cen ch\u00e9ng",
+    front_no_html: "\u5766\u627f   t\u01cen ch\u00e9ng",
+    back: "to confess, to admit",
+    spell: "t\u01cen ch\u00e9ng",
+    pinyin: "t\u01cen ch\u00e9ng",
+    hanzi: "\u5766\u627f",
+    hanzi_trad: "\u5766\u627f",
+    word: "\u5766\u627f",
+    card_id: "90576",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/349221.mp3"
+  },
+  {
+    front: "\u9752\u83dc<br>q\u012bngc\u00e0i",
+    front_no_html: "\u9752\u83dc   q\u012bngc\u00e0i",
+    back: "green vegetables, Chinese cabbage",
+    spell: "q\u012bngc\u00e0i",
+    pinyin: "q\u012bngc\u00e0i",
+    hanzi: "\u9752\u83dc",
+    hanzi_trad: "\u9752\u83dc",
+    word: "\u9752\u83dc",
+    card_id: "2650",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/46134.mp3"
+  },
+  {
+    front:
+      '\u8fb9\u754c <span class="trad_memo">\u908a\u754c</span> <br>bi\u0101nji\u00e8',
+    front_no_html: "\u8fb9\u754c   bi\u0101nji\u00e8",
+    back: "boundary, border",
+    spell: "bi\u0101nji\u00e8",
+    pinyin: "bi\u0101nji\u00e8",
+    hanzi: "\u8fb9\u754c",
+    hanzi_trad: "\u908a\u754c",
+    word: "\u8fb9\u754c",
+    card_id: "18925",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/237794.mp3"
+  },
+  {
+    front: "\u964d\u843d<br>ji\u00e0nglu\u00f2",
+    front_no_html: "\u964d\u843d   ji\u00e0nglu\u00f2",
+    back: "to descend, to land",
+    spell: "ji\u00e0nglu\u00f2",
+    pinyin: "ji\u00e0nglu\u00f2",
+    hanzi: "\u964d\u843d",
+    hanzi_trad: "\u964d\u843d",
+    word: "\u964d\u843d",
+    card_id: "9981",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/100021.mp3"
+  },
+  {
+    front:
+      '\u4ee4\u4eba\u5403\u60ca <span class="trad_memo">\u4ee4\u4eba\u5403\u9a5a</span> <br>l\u00ecng r\u00e9n ch\u012b j\u012bng',
+    front_no_html:
+      "\u4ee4\u4eba\u5403\u60ca   l\u00ecng r\u00e9n ch\u012b j\u012bng",
+    back: "to shock, to amaze",
+    spell: "l\u00ecng r\u00e9n ch\u012b j\u012bng",
+    pinyin: "l\u00ecng r\u00e9n ch\u012b j\u012bng",
+    hanzi: "\u4ee4\u4eba\u5403\u60ca",
+    hanzi_trad: "\u4ee4\u4eba\u5403\u9a5a",
+    word: "\u4ee4\u4eba\u5403\u60ca",
+    card_id: "91373",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/374138.mp3"
+  },
+  {
+    front: "\u501f\u9152\u6d88\u6101<br>ji\u00e8 ji\u01d4 xi\u0101o ch\u00f3u",
+    front_no_html:
+      "\u501f\u9152\u6d88\u6101   ji\u00e8 ji\u01d4 xi\u0101o ch\u00f3u",
+    back: "Take to drinking to forget one's sorrows",
+    spell: "ji\u00e8 ji\u01d4 xi\u0101o ch\u00f3u",
+    pinyin: "ji\u00e8 ji\u01d4 xi\u0101o ch\u00f3u",
+    hanzi: "\u501f\u9152\u6d88\u6101",
+    hanzi_trad: "\u501f\u9152\u6d88\u6101",
+    word: "\u501f\u9152\u6d88\u6101",
+    card_id: "88406",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/647477.mp3"
+  },
+  {
+    front:
+      '\u767b\u8bb0 <span class="trad_memo">\u767b\u8a18</span> <br>d\u0113ngj\u00ec',
+    front_no_html: "\u767b\u8bb0   d\u0113ngj\u00ec",
+    back: "to register (one's name)",
+    spell: "d\u0113ngj\u00ec",
+    pinyin: "d\u0113ngj\u00ec",
+    hanzi: "\u767b\u8bb0",
+    hanzi_trad: "\u767b\u8a18",
+    word: "\u767b\u8bb0",
+    card_id: "11270",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/112939.mp3"
+  },
+  {
+    front:
+      '\u7231\u5c14\u5170 <span class="trad_memo">\u611b\u723e\u862d</span> <br>\u00c0i \u011br l\u00e1n',
+    front_no_html: "\u7231\u5c14\u5170   \u00c0i \u011br l\u00e1n",
+    back: "Ireland",
+    spell: "\u00c0i \u011br l\u00e1n",
+    pinyin: "\u00c0i \u011br l\u00e1n",
+    hanzi: "\u7231\u5c14\u5170",
+    hanzi_trad: "\u611b\u723e\u862d",
+    word: "\u7231\u5c14\u5170",
+    card_id: "84872",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/361548.mp3"
+  },
+  {
+    front:
+      '\u817c\u8146 <span class="trad_memo">\u9766\u8146</span> <br>mi\u01centi\u01cen',
+    front_no_html: "\u817c\u8146   mi\u01centi\u01cen",
+    back: "shy, bashful",
+    spell: "mi\u01centi\u01cen",
+    pinyin: "mi\u01centi\u01cen",
+    hanzi: "\u817c\u8146",
+    hanzi_trad: "\u9766\u8146",
+    word: "\u817c\u8146",
+    card_id: "3660",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/45710.mp3"
+  },
+  {
+    front:
+      '\u7b80\u4ecb <span class="trad_memo">\u7c21\u4ecb</span> <br>ji\u01cenji\u00e8',
+    front_no_html: "\u7b80\u4ecb   ji\u01cenji\u00e8",
+    back: "summary, brief introduction",
+    spell: "ji\u01cenji\u00e8",
+    pinyin: "ji\u01cenji\u00e8",
+    hanzi: "\u7b80\u4ecb",
+    hanzi_trad: "\u7c21\u4ecb",
+    word: "\u7b80\u4ecb",
+    card_id: "16250",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/190764.mp3"
+  },
+  {
+    front:
+      '\u8eb2\u5f00 <span class="trad_memo">\u8eb2\u958b</span> <br>du\u01d2k\u0101i',
+    front_no_html: "\u8eb2\u5f00   du\u01d2k\u0101i",
+    back: "to stay out of (hot water, trouble, awkward situation etc)",
+    spell: "du\u01d2k\u0101i",
+    pinyin: "du\u01d2k\u0101i",
+    hanzi: "\u8eb2\u5f00",
+    hanzi_trad: "\u8eb2\u958b",
+    word: "\u8eb2\u5f00",
+    card_id: "13462",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/127894.mp3"
+  },
+  {
+    front:
+      '\u5f53\u4e2d <span class="trad_memo">\u7576\u4e2d</span> <br>d\u0101ngzh\u014dng',
+    front_no_html: "\u5f53\u4e2d   d\u0101ngzh\u014dng",
+    back: "among, in the middle",
+    spell: "d\u0101ngzh\u014dng",
+    pinyin: "d\u0101ngzh\u014dng",
+    hanzi: "\u5f53\u4e2d",
+    hanzi_trad: "\u7576\u4e2d",
+    word: "\u5f53\u4e2d",
+    card_id: "2265",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/46298.mp3"
+  },
+  {
+    front:
+      '\u9886\u57df <span class="trad_memo">\u9818\u57df</span> <br>l\u01d0ngy\u00f9',
+    front_no_html: "\u9886\u57df   l\u01d0ngy\u00f9",
+    back: "domain, sphere, field",
+    spell: "l\u01d0ngy\u00f9",
+    pinyin: "l\u01d0ngy\u00f9",
+    hanzi: "\u9886\u57df",
+    hanzi_trad: "\u9818\u57df",
+    word: "\u9886\u57df",
+    card_id: "9434",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/98531.mp3"
+  },
+  {
+    front:
+      '\u4e71\u52a8 <span class="trad_memo">\u4e82\u52d5</span> <br>lu\u00e0nd\u00f2ng',
+    front_no_html: "\u4e71\u52a8   lu\u00e0nd\u00f2ng",
+    back: "to fiddle with, to tamper with",
+    spell: "lu\u00e0nd\u00f2ng",
+    pinyin: "lu\u00e0nd\u00f2ng",
+    hanzi: "\u4e71\u52a8",
+    hanzi_trad: "\u4e82\u52d5",
+    word: "\u4e71\u52a8",
+    card_id: "538",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/42424.mp3"
+  },
+  {
+    front:
+      '\u7ea4\u7ef4 <span class="trad_memo">\u7e96\u7dad</span> <br>xi\u0101nw\u00e9i',
+    front_no_html: "\u7ea4\u7ef4   xi\u0101nw\u00e9i",
+    back: "fiber, CL:\u7a2e|\u79cd[zh\u01d2ng]",
+    spell: "xi\u0101nw\u00e9i",
+    pinyin: "xi\u0101nw\u00e9i",
+    hanzi: "\u7ea4\u7ef4",
+    hanzi_trad: "\u7e96\u7dad",
+    word: "\u7ea4\u7ef4",
+    card_id: "7084",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/64532.mp3"
+  },
+  {
+    front: "\u53e3\u7f69<br>k\u01d2uzh\u00e0o",
+    front_no_html: "\u53e3\u7f69   k\u01d2uzh\u00e0o",
+    back: "mask (surgical etc)",
+    spell: "k\u01d2uzh\u00e0o",
+    pinyin: "k\u01d2uzh\u00e0o",
+    hanzi: "\u53e3\u7f69",
+    hanzi_trad: "\u53e3\u7f69",
+    word: "\u53e3\u7f69",
+    card_id: "15437",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/153365.mp3"
+  },
+  {
+    front:
+      '\u79bb\u804c <span class="trad_memo">\u96e2\u8077</span> <br>l\u00edzh\u00ed',
+    front_no_html: "\u79bb\u804c   l\u00edzh\u00ed",
+    back: "to retire, to leave office",
+    spell: "l\u00edzh\u00ed",
+    pinyin: "l\u00edzh\u00ed",
+    hanzi: "\u79bb\u804c",
+    hanzi_trad: "\u96e2\u8077",
+    word: "\u79bb\u804c",
+    card_id: "12303",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/117379.mp3"
+  },
+  {
+    front:
+      '\u4f18\u52bf <span class="trad_memo">\u512a\u52e2</span> <br>y\u014dush\u00ec',
+    front_no_html: "\u4f18\u52bf   y\u014dush\u00ec",
+    back: "superiority, dominance",
+    spell: "y\u014dush\u00ec",
+    pinyin: "y\u014dush\u00ec",
+    hanzi: "\u4f18\u52bf",
+    hanzi_trad: "\u512a\u52e2",
+    word: "\u4f18\u52bf",
+    card_id: "3099",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/42572.mp3"
+  },
+  {
+    front:
+      '\u8282\u4fed <span class="trad_memo">\u7bc0\u5109</span> <br>ji\u00e9ji\u01cen',
+    front_no_html: "\u8282\u4fed   ji\u00e9ji\u01cen",
+    back: "frugal, economical",
+    spell: "ji\u00e9ji\u01cen",
+    pinyin: "ji\u00e9ji\u01cen",
+    hanzi: "\u8282\u4fed",
+    hanzi_trad: "\u7bc0\u5109",
+    word: "\u8282\u4fed",
+    card_id: "17894",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/229515.mp3"
+  },
+  {
+    front: "\u8dbe\u7532<br>zh\u01d0 ji\u01ce",
+    front_no_html: "\u8dbe\u7532   zh\u01d0 ji\u01ce",
+    back: "toenail",
+    spell: "zh\u01d0 ji\u01ce",
+    pinyin: "zh\u01d0 ji\u01ce",
+    hanzi: "\u8dbe\u7532",
+    hanzi_trad: "\u8dbe\u7532",
+    word: "\u8dbe\u7532",
+    card_id: "85305",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/399077.mp3"
+  },
+  {
+    front: "\u9ad8\u7b49<br>g\u0101od\u011bng",
+    front_no_html: "\u9ad8\u7b49   g\u0101od\u011bng",
+    back: "higher, high level, advanced",
+    spell: "g\u0101od\u011bng",
+    pinyin: "g\u0101od\u011bng",
+    hanzi: "\u9ad8\u7b49",
+    hanzi_trad: "\u9ad8\u7b49",
+    word: "\u9ad8\u7b49",
+    card_id: "23020",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/259270.mp3"
+  },
+  {
+    front:
+      '\u4f20\u795e <span class="trad_memo">\u50b3\u795e</span> <br>chu\u00e1n sh\u00e9n',
+    front_no_html: "\u4f20\u795e   chu\u00e1n sh\u00e9n",
+    back: "vivid, lifelike",
+    spell: "chu\u00e1n sh\u00e9n",
+    pinyin: "chu\u00e1n sh\u00e9n",
+    hanzi: "\u4f20\u795e",
+    hanzi_trad: "\u50b3\u795e",
+    word: "\u4f20\u795e",
+    card_id: "90577",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/240926.mp3"
+  },
+  {
+    front: "\u8ff7\u7cca<br>m\u00ed hu",
+    front_no_html: "\u8ff7\u7cca   m\u00ed hu",
+    back: "muddle-headed, dazed",
+    spell: "m\u00ed hu",
+    pinyin: "m\u00ed hu",
+    hanzi: "\u8ff7\u7cca",
+    hanzi_trad: "\u8ff7\u7cca",
+    word: "\u8ff7\u7cca",
+    card_id: "84995",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/242666.mp3"
+  },
+  {
+    front:
+      '\u597d\u81ea\u4e3a\u4e4b <span class="trad_memo">\u597d\u81ea\u70ba\u4e4b</span> <br>h\u01ceoz\u00ecw\u00e9izh\u012b',
+    front_no_html: "\u597d\u81ea\u4e3a\u4e4b   h\u01ceoz\u00ecw\u00e9izh\u012b",
+    back: "to do one's best, to shape up",
+    spell: "h\u01ceoz\u00ecw\u00e9izh\u012b",
+    pinyin: "h\u01ceoz\u00ecw\u00e9izh\u012b",
+    hanzi: "\u597d\u81ea\u4e3a\u4e4b",
+    hanzi_trad: "\u597d\u81ea\u70ba\u4e4b",
+    word: "\u597d\u81ea\u4e3a\u4e4b",
+    card_id: "8847",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/97053.mp3"
+  },
+  {
+    front: "\u6301\u4e45<br>ch\u00edji\u01d4",
+    front_no_html: "\u6301\u4e45   ch\u00edji\u01d4",
+    back: "lasting, enduring, persistent",
+    spell: "ch\u00edji\u01d4",
+    pinyin: "ch\u00edji\u01d4",
+    hanzi: "\u6301\u4e45",
+    hanzi_trad: "\u6301\u4e45",
+    word: "\u6301\u4e45",
+    card_id: "3333",
+    front_audio_file: "//yabla.vo.llnwd.net/media.yabla.com/audio/46061.mp3"
+  }
+];
+
+let startGame = () => {
+  console.log("START GAME");
+  // hide init div, show game div
+  let initDiv = document.querySelector(".fill-blanks-init");
+  initDiv.classList.add("hidden");
+  let gameDiv = document.querySelector(".fill-blanks-game");
+  gameDiv.classList.remove("hidden");
+  // receive text
+  let textarea = document.querySelector(".fill-blanks-init-txt-input");
+  let text = textarea.value;
+  // process cards into hash
+  let cards = {};
+  CARDS.forEach((card) => {
+    cards[card.hanzi] = card;
+  });
+  let parsedText = [];
+  let last = 0;
+  // process text. elements are substrings of text or cards
+  for (let i = 1; i < text.length - 2; ++i) {
+    let substrLen2 = toUnicode(text.substring(i, i + 1));
+    let substrLen3 = toUnicode(text.substring(i, i + 2));
+    // if we find a valid word
+    if (cards[substrLen3] !== undefined) {
+      // we need to take everything before this as whole text
+      parsedText.add(text.substring(last, i - 1));
+      // save the card and move i past this
+      parsedText.add(cards[substrLen3]);
+      i += 2;
+      last = i + 1;
+    } else if (cards[substrLen2] !== undefined) {
+      parsedText.add(text.substring(last, i - 1));
+      parsedText.add(cards[substrLen2]);
+      i += 1;
+      last = i + 1;
+    }
+  }
+  parsedText.add(text.substring(last, text.length - 1));
+  /// HOW DO WE TAKE A CARD AND TURN IT INTO A FILL IN BLANK?
+};
+
+let startBtn = document.querySelector(".fill-blanks-start-btn");
+startBtn.addEventListener("click", startGame);
+
+/*
+PROCESS TEXT
+-process in groups of 2,3,4
+--at index i, take substring i,i+1 etc...
+
+HOW TO CONVERT HANZI FROM TEXT INTO URL ENCODED?
+IT ISNT URL ENCODED, IT IS UNICODE
+
+Cards is already processed
+{
+    "front": "\u5766\u627f<br>t\u01cen ch\u00e9ng",
+    "front_no_html": "\u5766\u627f   t\u01cen ch\u00e9ng",
+    "back": "to confess, to admit",
+    "spell": "t\u01cen ch\u00e9ng",
+    "pinyin": "t\u01cen ch\u00e9ng",
+    "hanzi": "\u5766\u627f",
+    "hanzi_trad": "\u5766\u627f",
+    "word": "\u5766\u627f",
+    "card_id": "90576",
+    "front_audio_file": "\/\/yabla.vo.llnwd.net\/media.yabla.com\/audio\/349221.mp3"
+},
+Lets use hanzi trad and pinyin to show the text
+-Ideally: on mouseover, any char will show simplified 
+How do we extract the useful functionality 
+Can we still get correct answers to count for correct resp in stats?
+*/
+function toUnicode(str) {
+  return str
+    .split("")
+    .map(function (value, index, array) {
+      var temp = value.charCodeAt(0).toString(16).toUpperCase();
+      if (temp.length > 2) {
+        return "\\u" + temp;
+      }
+      return value;
+    })
+    .join("");
+}
